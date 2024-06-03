8e45128b5efb3ddceddf40beb4e95f7e5ebfeae2
VishalG2001 <vishalguptalx@gmail.com>
2024-06-03 11:42:12 +0530

    selectlistener on viewholder

--- app/src/main/java/com/example/noteworthy/RVAdapter.java
+++ app/src/main/java/com/example/noteworthy/RVAdapter.java
@@ -14,7 +14,6 @@ import com.example.noteworthy.databinding.LayyBinding;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class RVAdapter extends RecyclerView.Adapter<RVAdapter.MyViewHolder> {
 
     private List<NoteModel> noteModelList = new ArrayList<>();
@@ -24,34 +23,20 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.MyViewHolder> {
         this.selectListener = selectListener;
     }
 
-    //    public RVAdapter(List<NoteModel> notes){
-//        this.notes = notes;
-//    }
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         LayyBinding binding = LayyBinding.inflate(inflater, parent, false);
-        return new MyViewHolder(binding,selectListener);
+        return new MyViewHolder(binding, selectListener);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder,  int position) {
-        // Bind data to the ViewHolder
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         NoteModel noteModel = noteModelList.get(position);
         holder.bind(noteModel);
-        holder.binding.idlayy.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent inn = new Intent(v.getContext(), MainActivity2.class);
-                inn.putExtra("ttle",noteModel.getTitle());
-                inn.putExtra("contnt",noteModel.getContent());
-                inn.putExtra("noteid",noteModel.getId());
-                v.getContext().startActivity(inn);
-            }
-        });
+        holder.setOnClickListener(noteModel);
     }
-
 
     @Override
     public int getItemCount() {
@@ -63,20 +48,20 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.MyViewHolder> {
         notifyDataSetChanged();
     }
 
-
     public static class MyViewHolder extends RecyclerView.ViewHolder {
         private LayyBinding binding;
-
+        private final SelectListener selectListener;
 
         public MyViewHolder(LayyBinding binding, SelectListener selectListener) {
             super(binding.getRoot());
             this.binding = binding;
+            this.selectListener = selectListener;
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if(selectListener!= null){
+                    if (selectListener != null) {
                         int pos = getAdapterPosition();
-                        if (pos != RecyclerView.NO_POSITION){
+                        if (pos != RecyclerView.NO_POSITION) {
                             selectListener.onItemClicked(pos);
                         }
                     }
@@ -85,12 +70,21 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.MyViewHolder> {
         }
 
         public void bind(NoteModel noteModel) {
-            // Bind data to the views
             binding.textViewUpper.setText(noteModel.getTitle());
             binding.textViewLower.setText(noteModel.getContent());
+        }
 
-
+        public void setOnClickListener(NoteModel noteModel) {
+            binding.idlayy.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent inn = new Intent(v.getContext(), MainActivity2.class);
+                    inn.putExtra("ttle", noteModel.getTitle());
+                    inn.putExtra("contnt", noteModel.getContent());
+                    inn.putExtra("noteid", noteModel.getId());
+                    v.getContext().startActivity(inn);
+                }
+            });
         }
     }
 }
-
